efdbaf530f76f11e085248d8fdd7ed58ba073538
maricel <maricelmb@gmail.com>
2022-07-26 08:51:05 +0800

    S10L133_NestedRoutes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         {/* 'Index' will display if '/' is called */}
         <Route index element={<Home />} />
-        <Route path="shop" element={<Shop />} />
+        <Route path="shop/*" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
         <Route path='checkout' element={<Checkout />} />
       </Route>
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,22 +1,11 @@
-import { useContext } from "react";
-import { CategoriesContext } from "../../contexts/categories.context";
-import ProductCard from "../../components/product-card/product-card.component";
-import CategoryPreview from "../../components/category-preview/category-preview.component";
-import "./shop.styles.scss";
-import { Fragment } from "react/cjs/react.production.min";
+import { Routes, Route } from "react-router-dom";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
-
   return (
-    <div>
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview key={title} title={title} products={products} />
-        );
-      })}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />}></Route>
+    </Routes>
   );
 };
 
