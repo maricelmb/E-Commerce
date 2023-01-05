840c568b010186e9e6180c318cf32bb87c86f7af
maricel <maricelmb@gmail.com>
2023-01-05 15:54:30 +0800

    L152_Setting up a store

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -12,29 +12,6 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
-export const USER_ACTION_TYPES = {
-  SET_CURRENT_USER: "SET_CURRENT_USER",
-};
-
-const userReducer = (state, action) => {
-  // console.log("dispatched");
-  // console.log(action);
-  const { type, payload } = action;
-
-  switch (type) {
-    case USER_ACTION_TYPES.SET_CURRENT_USER:
-      return {
-        ...state,
-        currentUser: payload,
-      };
-    default:
-      throw new Error("Unhandled type ${type} in userReducer");
-  }
-};
-
-const INITIAL_STATE = {
-  currentUser: null,
-};
 
 export const UserProvider = ({ children }) => {
   //const [currentUser, setCurrentUser] = useState(null);
---
+++ src/utils/store/user/user.reducer.js
@@ -0,0 +1,24 @@
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  // console.log("dispatched");
+  // console.log(action);
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      //throw new Error("Unhandled type ${type} in userReducer");
+      return state;
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
